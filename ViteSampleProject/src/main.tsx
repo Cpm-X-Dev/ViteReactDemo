92e29fc041206dcd19161dc17b676e9f31990967
Christian Paul Mendoza <pm.christian@jtrit.com>
2026-03-05 14:01:35 +0800

    feat: added contact page

--- ViteSampleProject/src/main.tsx
+++ ViteSampleProject/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App'
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from './pages/Contact'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route element={<App />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
